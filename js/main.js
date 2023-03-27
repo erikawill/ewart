86a8dc8f1c7f54e3513ca41fa5f4c96a2cb71a53
erikawill <111911682+erikawill@users.noreply.github.com>
2023-03-26 23:07:52 -0500

    changes
    - removed social media icons
    - added roboto font
    - added buttons, border, and logo to other pages, but it's not lined up correctly and the font isn't showing up on the other pages

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 // JS scripts placed here
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
+ 
