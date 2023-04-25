c01e2ee8a5d2ae1e49b698ff96f86fdde50d203f
erikawill <111911682+erikawill@users.noreply.github.com>
2023-04-25 13:04:37 -0500

    changes [tues apr 25, 2023]
    - added buttons
    -added contact form, but i'm gonna change the background

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 // JS scripts placed here
 document.querySelector('#contact-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    e.target.elements.name.value = '';
-    e.target.elements.email.value = '';
-    e.target.elements.message.value = '';
-  });
- 
+  e.preventDefault();
+  e.target.elements.name.value = '';
+  e.target.elements.email.value = '';
+  e.target.elements.message.value = '';
+});
